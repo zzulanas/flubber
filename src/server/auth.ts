43c010f52b61b9a20fe48977bbfe773f65836d70
zzulanas <zzulanas@gmail.com>
2023-02-17 12:49:23 -0500

    auth cycle now working, some issues with deadlock writes I think (need to investigate later)

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,13 +5,14 @@ import {
   getServerSession,
   type NextAuthOptions,
   type DefaultSession,
-  type JWT,
 } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "../env/server.mjs";
 import { prisma } from "./db";
 import { LOGIN_URL, spotifyApi } from "./lib/spotify.js";
+import { json } from "stream/consumers";
+import { Session } from "next-auth/core/types.js";
 
 /**
  * Module augmentation for `next-auth` types.
@@ -52,13 +53,18 @@ declare module "next-auth" {
  **/
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    session({ session, user }) {
+    async session({ session, user }) {
+      session.user.id = user.id
+      const accessTokenRefresh = await refreshAccessToken(session)
+      if(accessTokenRefresh){
+        session.user.accessToken = accessTokenRefresh?.access_token ? accessTokenRefresh?.access_token : session.user.accessToken
+      }
       session.user.id = user.id;
       return session;
     },
     jwt({token, account}) {
       if (account) {
-        token.accessToken = account.refresh_token;
+        token.accessToken = refreshAccessToken
       }
       return token;
     },
@@ -82,6 +88,66 @@ export const authOptions: NextAuthOptions = {
   ],
   secret: process.env.JWT_SECRET,
 };
+
+async function refreshAccessToken(newSession: Session) : Promise<{ access_token: string | null; refresh_token: string | null | undefined; } | undefined> {
+  try {
+    const userAccount = await prisma.account.findFirst({
+      where: { userId: newSession.user.id },
+    });
+
+
+    if (userAccount?.expires_at) {
+      if ((Date.now()/1000) < userAccount?.expires_at) {
+        console.log(`Token not refreshed for user ${userAccount.id}`)
+        console.log(`Token expires at ${userAccount.expires_at}`)
+        return {
+          access_token: userAccount.access_token,
+          refresh_token: userAccount.refresh_token,
+        };
+      }
+    }
+
+    const refresh_token = userAccount?.refresh_token;
+
+    spotifyApi.setRefreshToken(refresh_token)
+    const access_response = await spotifyApi.refreshAccessToken()
+
+    if (!(access_response.statusCode == 200)) {
+      console.error("Error refreshing auth token")
+    }
+
+
+    await prisma.account.update({
+      where: { id: userAccount?.id },
+      data: {
+        refresh_token: access_response.body.refresh_token ?? refresh_token,
+        access_token: access_response.body.access_token,
+        expires_at: Math.floor(new Date().getTime() / 1000) + 3600
+      },
+    });
+    
+    if(userAccount?.id && userAccount?.expires_at){
+      console.log(`Token refreshed for user ${userAccount.id}`)
+      console.log(`Token expires at ${userAccount.expires_at}`)
+    }
+  
+    const response: { access_token: string | null; refresh_token: string | null| undefined; } ={
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      access_token: access_response.body.access_token ?? refresh_token,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      refresh_token: access_response.body.refresh_token ? access_response.body.refresh_token : refresh_token
+    }
+
+    return new Promise((resolve) => {
+      resolve(response)
+    })
+  } catch (error) {
+    console.log(error);
+  }
+  return new Promise((resolve) => {
+    resolve(undefined)
+  })
+}
 
 /**
  * Wrapper for `getServerSession` so that you don't need to import the
